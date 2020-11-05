d27efb300c3bd9718a953b30d086a3ec2b37214b
gdarko <dg@darkog.com>
2020-11-05 15:46:39 +0000

    Improved local carbon time calculation

--- src/Misc/CarbonLocal.php
+++ src/Misc/CarbonLocal.php
@@ -4,22 +4,37 @@
 namespace The7055inc\Shared\Misc;
 
 
-use Carbon\Carbon;
+use Carbon\Carbon as parentAlias;
 
 /**
  * Class CarbonLocal
  * @package The7055inc\Shared\Misc
  */
-class CarbonLocal extends Carbon
-{
-    public function __construct($time = null, $tz = null)
-    {
-        parent::__construct($time, $tz);
+class CarbonLocal extends parentAlias {
+	public function __construct( $time = null, $tz = null ) {
+		parent::__construct( $time, $tz );
 
-        $timezone = wp_timezone();
+		$timezone = wp_timezone();
 
-        if ( ! empty($timezone)) {
-            $this->setTimezone($timezone);
-        }
-    }
+		if ( ! empty( $timezone ) ) {
+			$this->setTimezone( $timezone );
+		}
+	}
+
+	/**
+	 * Create from format
+	 *
+	 * @param string $format
+	 * @param string $time
+	 * @param \DateTimeZone|null $timezone
+	 *
+	 * @return parentAlias|\DateTime|false|CarbonLocal|void
+	 */
+	public static function createFromFormat( $format, $time, $timezone = null ) {
+		if ( is_null( $timezone ) ) {
+			$timezone = wp_timezone();
+		}
+
+		return parent::createFromFormat( $format, $time, $timezone );
+	}
 }
